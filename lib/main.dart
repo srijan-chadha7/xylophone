710991e1585704aac4caf8dbe6d7b60b849c1217
srijan-chadha7 <srijan.chadha14@gmail.com>
2020-07-23 18:08:46 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.indigo,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note1.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note2.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note3.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note4.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note5.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note6.wav');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.black,
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note7.wav');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
